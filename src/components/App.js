dcc39e8c057fcd89889fa70590febb7c33b69cfa
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-06-06 18:37:05 +0600

    Feat: upgrade the router to the v6.23 to use the route loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet, RouterProvider } from "react-router-dom";
 
-import About from "../pages/About";
-import Blog from "../pages/Blog";
-import Categories from "../pages/Categories";
-import CategoryPage from "../pages/CategoryPage";
-import Contact from "../pages/Contact";
-import Home from "../pages/home";
-import SearchResult from "../pages/search-results";
-import SingleBook from "../pages/single-book";
+import { router } from "../router";
 import Footer from "./footer";
 import Header from "./header";
 
-function App() {
+export function Root() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="search-result" element={<SearchResult />} />
-          <Route path="/:title/ID=:id" element={<SingleBook />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/:categoryTitle" element={<CategoryPage />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      {/* <Routes>
+        <Route path="search-result" element={<SearchResult />} />
+        <Route path="/:title/ID=:id" element={<SingleBook />} />
+
+        <Route path="/blog" element={<Blog />} />
+      </Routes> */}
+      <Outlet />
+      <Footer />
     </>
   );
+}
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Root } from "../components/App";
+import Home from "../pages/home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Categories from "../pages/Categories";
+import CategoryPage from "../pages/CategoryPage";
+import SearchResult from "../pages/search-results";
+import SingleBook from "../pages/single-book";
+import { singleBookLoader } from "../loaders/singleBookLoader";
+import { categoryProductsLoader } from "../loaders/categoryProductsLoader";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", index: true, element: <Home /> },
+      { path: "about-us", element: <About /> },
+      { path: "contact-us", element: <Contact /> },
+      { path: "categories", element: <Categories /> },
+      {
+        path: "categories/:categoryTitle",
+        element: <CategoryPage />,
+        loader: categoryProductsLoader,
+      },
+      { path: "search-result", element: <SearchResult /> },
+      { path: ":title", element: <SingleBook />, loader: singleBookLoader },
+    ],
+  },
+]);
